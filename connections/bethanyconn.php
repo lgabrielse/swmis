fbb127ec35c4d0618a5cba67e912fc387cecfd37
Len Gabrielse <lengabrielse@gmail.com>
2019-01-02 12:15:03 -0500

    commit connections

---
+++ connections/bethanyconn.php
@@ -0,0 +1,13 @@
+<?php
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+
+    error_reporting(E_ALL ^ E_DEPRECATED);  //stop error message about mysqli
+	$hostname_swmisconn = "localhost";
+	$database_swmisconn = "swmisbethany";
+	$username_swmisconn = "root";
+	$password_swmisconn = "jiloa7";
+	$swmisconn = mysql_pconnect($hostname_swmisconn, $username_swmisconn, $password_swmisconn) or trigger_error(mysql_error(),E_USER_ERROR); 
+	//$swmisconn = mysql_pconnect($hostname_bethanyconn, $username_bethanyconn, $password_bethanyconn) or trigger_error(mysql_error(),E_USER_ERROR); 
+	?>
